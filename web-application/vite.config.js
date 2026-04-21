6b284bb21e7309eaeface81e504db379812eae2d
Waldemar Tkatskiy <130499580+gaspacho2103@users.noreply.github.com>
2026-04-21 06:53:14 +0400

    Update vite.config.js

--- web-application/vite.config.js
+++ web-application/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/galaxy/", // ⚠️ Важно: должен совпадать с basename в Router
+  base: "/galaxy/", // Должен совпадать с basename в Router
 });
